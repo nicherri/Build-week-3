186ec23171871a36b7daf34a929d95784252a4f2
Nicola Schirripa <164496803+nicherri@users.noreply.github.com>
2024-06-10 16:47:56 +0200

    home

--- PizzArray/src/app/app-routing.module.ts
+++ PizzArray/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'Auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [{ path: 'Auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'Home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- PizzArray/src/app/app.module.ts
+++ PizzArray/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './main-components/navbar/navbar.component';
+import { FooterComponent } from './main-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
