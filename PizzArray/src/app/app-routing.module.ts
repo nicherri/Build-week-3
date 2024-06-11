530cf89e253b36003d655d76a38b3bf099b20a15
Catenacci Nicola <catenaccinicola@outlook.it>
2024-06-11 16:07:30 +0200

    router

--- PizzArray/src/app/app-routing.module.ts
+++ PizzArray/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaRicetteComponent } from './Pages/lista-ricette/lista-ricette.component';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'crea-ricetta', loadChildren: () => import('./crea-ricetta/crea-ricetta.module').then(m => m.CreaRicettaModule) },
+  { path: 'lista-ricette',
+    component: ListaRicetteComponent
+   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' }
 ];
