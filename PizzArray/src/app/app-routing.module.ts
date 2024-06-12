7450de9e0510db37d0130ed3784f7413bf2c3543
mariantoniettaangelini <164540376+mariantoniettaangelini@users.noreply.github.com>
2024-06-12 15:57:54 +0200

    Risoluzione merge

--- PizzArray/src/app/app-routing.module.ts
+++ PizzArray/src/app/app-routing.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< Updated upstream
-
-const routes: Routes = [
-  { path: 'Auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'Home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'crea-ricetta', loadChildren: () => import('./crea-ricetta/crea-ricetta.module').then(m => m.CreaRicettaModule) }];
-=======
 import { ListaRicetteComponent } from './Pages/lista-ricette/lista-ricette.component';
 import { GuestGuard } from './auth/guest.guard';
 import { AuthGuard } from './auth/auth.guard';
@@ -40,7 +33,6 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' }
 ];
->>>>>>> Stashed changes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
