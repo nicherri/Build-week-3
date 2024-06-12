99ff77521faf18619e507ec707ee8950d671f90e
Nicola Schirripa <164496803+nicherri@users.noreply.github.com>
2024-06-12 11:37:25 +0200

    Pagina ingresienti provvisoria

--- PizzArray/src/app/Pages/lista-ingredienti/lista-ingredienti.component.ts
+++ PizzArray/src/app/Pages/lista-ingredienti/lista-ingredienti.component.ts
@@ -1,22 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { iIngredient } from '../../Models/i-ingredient';
 import { RecipeService } from '../../services/recipe.service';
-
 
 @Component({
   selector: 'app-lista-ingredienti',
   templateUrl: './lista-ingredienti.component.html',
-  styleUrl: './lista-ingredienti.component.scss'
+  styleUrls: ['./lista-ingredienti.component.scss']
 })
-export class ListaIngredientiComponent {
+export class ListaIngredientiComponent implements OnInit {
+  ingredients: iIngredient[] = [];
+  displayedIngredients: iIngredient[] = [];
+  itemsPerPage: number = 10;
+  page: number = 1;
+  totalItems: number = 0;
 
-  ingredients:iIngredient[]=[]
+  constructor(private recipeSvc: RecipeService) {}
 
-  constructor(private recipeSvc:RecipeService){}
+  ngOnInit(): void {
+    this.recipeSvc.getIngredients().subscribe(
+      ingr => {
+        console.log('Ingredients fetched:', ingr);
+        this.ingredients = ingr;
+        this.totalItems = this.ingredients.length;
+        this.updatePagination();
+      },
+      error => {
+        console.error('Error fetching ingredients:', error);
+      }
+    );
+  }
 
-  ngOnInit(){
-    this.recipeSvc.getIngredients().subscribe(ingr=>{console.log(ingr)
-      this.ingredients=ingr
-    })
+  updatePagination(): void {
+    const startIndex = (this.page - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.displayedIngredients = this.ingredients.slice(startIndex, endIndex);
+    console.log('Displayed Ingredients:', this.displayedIngredients);
+  }
+
+  selectPage(page: any): void {
+    const pageNumber = Number(page);
+    if (!isNaN(pageNumber) && pageNumber > 0 && pageNumber <= Math.ceil(this.totalItems / this.itemsPerPage)) {
+      this.page = pageNumber;
+      this.updatePagination();
+      console.log('Page selected:', this.page);
+    }
+  }
+
+  formatInput(target: EventTarget | null): void {
+    const input = target as HTMLInputElement;
+    let value = input.value;
+    value = value.replace(/[^0-9]/g, '');
+    input.value = value;
   }
 }
